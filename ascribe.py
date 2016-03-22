51aa78e799fc271b5c233a302bd811a196d95b3c
Sylvain Bellemare <sylvain@ascribe.io>
2016-03-22 11:16:51 +0100

    Add dummy GET for /piece and re-organize code

--- ascribe.py
+++ ascribe.py
@@ -4,9 +4,6 @@ import json
 import requests
 import os
 
-
-import qrcode
-
 from docutils import nodes
 from docutils.parsers import rst
 
@@ -39,6 +36,10 @@ from rinohlib.stylesheets.matcher import matcher
 from flask import Flask, request, send_file
 from jinja2 import Environment, PackageLoader
 
+import dummies
+import qrcode
+
+
 app = Flask(__name__)
 app.config.from_object(
     os.environ.get('ASCRIBEPDF_CONFIG_MODULE', 'config.Prod'))
@@ -50,23 +51,6 @@ jinja_env = Environment(loader=PackageLoader(__name__))
 
 DEFAULT_TEMPLATE_FILENAME = 'edition.rst'
 TEMPLATE_DIAMOND = jinja_env.get_template('diamond.rst')
-
-# sample JSON input
-data_test = {'title': '111111111', 'filesize': 370611, 'edition_number': 10,
-             'check_stamp_query': '?message=1111111111%2A111111111%2A10%2F13%2A2015%2A2015Jul29-16%3A21%3A51&signature=2CEC38ACD51F91D0A38CF85E8092C337CC18C4ADE1E0F86563D28A97C0442F9EF9202E25E95D85BB1BA5AC3FB18E290E51BC9D1A410D065C7EF716168A13C44F7FA1431404367A4934ECCCE3AA0DA6921A908909B0134A10D4CDBD372C53967FD02760B99FDBE21E1DEB64532305351CFB35E0245B26D54682CAF01F359DD0EDL',
-             'owner_timestamp': 'Jul. 29 2015, 16:21:51', 'artist_name': '1111111111',
-             'digital_work': {'isEncoding': 0, 'mime': 'image', 'hash': '16a78cd2bb628fd6fb14582668ed72d7',
-                              'encoding_urls': None, 'id': 2509,
-                              'url_safe': 'https://d1qjsxua1o9x03.cloudfront.net/live%2Fb6d767d2f8ed5d21a44b0e5886680cb9%2F589566d5-5452-48ef-a7c9-88f5f4aefb86%2Fdigitalwork%2F589566d5-5452-48ef-a7c9-88f5f4aefb86.png',
-                              'url': 'https://d1qjsxua1o9x03.cloudfront.net/live/b6d767d2f8ed5d21a44b0e5886680cb9/589566d5-5452-48ef-a7c9-88f5f4aefb86/digitalwork/589566d5-5452-48ef-a7c9-88f5f4aefb86.png'},
-             'bitcoin_id': '1FE7GhESMTN5tUS3TSma4CqTtgZVTV9fnR',
-             'thumbnail': 'https://d1qjsxua1o9x03.cloudfront.net/media%2Fthumbnails%2Fc681cdee-f852-4ade-9179-61da68bf7a45%2Fmothership01.jpg',
-             'owner': 'Schweinehund', 'crypto_message': '1111111111*111111111*10/13*2015*2015Jul29-16:21:51',
-             'crypto_signature': '2CEC38ACD51F91D0A38CF85E8092C337CC18C4ADE1E0F86563D28A97C0442F9EF9202E25E95D85BB1BA5AC3FB18E290E51BC9D1A410D065C7EF716168A13C44F7FA1431404367A4934ECCCE3AA0DA6921A908909B0134A10D4CDBD372C53967FD02760B99FDBE21E1DEB64532305351CFB35E0245B26D54682CAF01F359DD0EDL',
-             'yearAndEdition_str': '2015, 10/13', 'filename': '.png',
-             'check_stamp_url': 'http://localhost.com:3000/coa_verify/?message=1111111111%2A111111111%2A10%2F13%2A2015%2A2015Jul29-16%3A21%3A51&signature=2CEC38ACD51F91D0A38CF85E8092C337CC18C4ADE1E0F86563D28A97C0442F9EF9202E25E95D85BB1BA5AC3FB18E290E51BC9D1A410D065C7EF716168A13C44F7FA1431404367A4934ECCCE3AA0DA6921A908909B0134A10D4CDBD372C53967FD02760B99FDBE21E1DEB64532305351CFB35E0245B26D54682CAF01F359DD0EDL',
-             'ownership_history': [['Jul. 29, 2015, 16:21:51', 'Registered by Schweinehund']], 'num_editions': 13,
-             'verify_owner_url': 'http://localhost.com:3000/coa_verify/'}
 
 
 class qrimage(nodes.image):
@@ -392,14 +376,21 @@ def render_and_send_certificate_diamond(data):
     return response
 
 
-@app.route('/piece', methods=['POST'])
+@app.route('/piece', methods=['GET', 'POST'])
 def generate_piece_certificate():
-    try:
-        return render_and_send_certificate(request.json,
-                                           template_filename='piece.rst')
-    except Exception as e:
-        # TODO use logging
-        print(e)
+    if request.method == 'GET':
+        try:
+            return render_and_send_certificate(dummies.piece)
+        except Exception as e:
+            # TODO use logging
+            print('Error: ' + str(e))
+    else:
+        try:
+            return render_and_send_certificate(request.json,
+                                               template_filename='piece.rst')
+        except Exception as e:
+            # TODO use logging
+            print(e)
 
 
 @app.route('/', methods=['POST'])
@@ -420,7 +411,7 @@ def generate_edition_certificate():
 @app.route('/', methods=['GET'])
 def test():
     try:
-        return render_and_send_certificate(data_test)
+        return render_and_send_certificate(dummies.edition)
     except Exception as e:
         print('Error: ' + str(e))
 
@@ -439,6 +430,5 @@ def certificate_diamond():
 
 
 if __name__ == "__main__":
-    # render_certificate(data_test, to_file=True
     host = os.environ.get('ASCRIBEPDF_HOST', '127.0.0.1')
     app.run(host=host)
